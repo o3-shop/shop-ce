7b58d6de90ad73dc979a531ebdb4b1f8af6e84bd
O3-Shop <o3s@ist-einmalig.de>
2023-03-21 00:10:40 +0100

    0000001: add database version checkers

---
+++ source/Internal/Framework/Database/CompatibilityChecker/MariaDbChecker.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * This file is part of O3-Shop.
+ *
+ * O3-Shop is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * O3-Shop is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with O3-Shop.  If not, see <http://www.gnu.org/licenses/>
+ *
+ * @copyright  Copyright (c) 2022 O3-Shop (https://www.o3-shop.com)
+ * @license    https://www.gnu.org/licenses/gpl-3.0  GNU General Public License 3 (GPLv3)
+ */
+
+declare(strict_types=1);
+
+namespace OxidEsales\EshopCommunity\Internal\Framework\Database\CompatibilityChecker;
+
+class MariaDbChecker implements MariaDbCheckerInterface, DatabaseCheckerInterface
+{
+    protected string $versionString;
+    protected string $version;
+
+    /**
+     * @param string $version
+     *
+     * @return void
+     */
+    public function setVersion(string $version): void
+    {
+        $this->versionString = $version;
+        $this->version = substr($this->versionString, 0, strpos($this->versionString, '-'));
+    }
+
+    /**
+     * @return bool
+     */
+    public function isAllowed(): bool
+    {
+        return
+            version_compare($this->version, '10.4', '>=') &&
+            version_compare($this->version, '10.5', '<');
+    }
+}
