d65e5c9e888956b0471babc96b3a5235e1d4ff9e
O3-Shop <o3-shop-github@ist-einmalig.de>
2023-03-24 09:02:20 +0100

    dont decode unencoded config values

--- source/migration/data/Version20230322214524.php
+++ source/migration/data/Version20230322214524.php
@@ -24,25 +24,24 @@ declare(strict_types=1);
 namespace OxidEsales\EshopCommunity\Migrations;
 
 use Doctrine\DBAL\Schema\Schema;
-use Doctrine\DBAL\Types\BlobType;
 use Doctrine\Migrations\AbstractMigration;
 use OxidEsales\EshopCommunity\Core\Config;
 
-final class Version20230322213324 extends AbstractMigration
+final class Version20230322214524 extends AbstractMigration
 {
     public function up(Schema $schema): void
     {
         $this->connection->getDatabasePlatform()->registerDoctrineTypeMapping('enum', 'string');
 
-        $table = $schema->getTable('oxconfig');
-        $column = $table->getColumn('oxvarvalue');
+        $table = $schema->getTable('oxuserpayments');
+        $column = $table->getColumn('oxvalue');
 
         if ($column->getType() instanceof BlobType) {
-            $this->addSql('ALTER TABLE oxconfig ADD COLUMN `OXVARVALUE_UNENC` text;');
-            $this->addSql("UPDATE oxconfig SET `OXVARVALUE_UNENC` = DECODE(OXVARVALUE, '".Config::DEFAULT_CONFIG_KEY."') WHERE 1;");
-            $this->addSql('ALTER TABLE oxconfig MODIFY COLUMN `OXVARVALUE` text;');
-            $this->addSql("UPDATE oxconfig SET `OXVARVALUE` = `OXVARVALUE_UNENC` WHERE 1;");
-            $this->addSql('ALTER TABLE oxconfig DROP COLUMN `OXVARVALUE_UNENC`;');
+            $this->addSql('ALTER TABLE oxuserpayments ADD COLUMN `OXVALUE_UNENC` text;');
+            $this->addSql("UPDATE oxuserpayments SET `OXVALUE_UNENC` = DECODE(OXVALUE, '".Config::DEFAULT_CONFIG_KEY."') WHERE 1;");
+            $this->addSql('ALTER TABLE oxuserpayments MODIFY COLUMN `OXVALUE` text;');
+            $this->addSql("UPDATE oxuserpayments SET `OXVALUE` = `OXVALUE_UNENC` WHERE 1;");
+            $this->addSql('ALTER TABLE oxuserpayments DROP COLUMN `OXVALUE_UNENC`;');
         }
     }
 
@@ -50,15 +49,15 @@ final class Version20230322213324 extends AbstractMigration
     {
         $this->connection->getDatabasePlatform()->registerDoctrineTypeMapping('enum', 'string');
 
-        $table = $schema->getTable('oxconfig');
-        $column = $table->getColumn('oxvarvalue');
+        $table = $schema->getTable('oxuserpayments');
+        $column = $table->getColumn('oxvalue');
 
         if (!$column->getType() instanceof BlobType) {
-            $this->addSql( 'ALTER TABLE oxconfig ADD COLUMN `OXVARVALUE_ENC` text;' );
-            $this->addSql( "UPDATE oxconfig SET `OXVARVALUE_ENC` = ENCODE(OXVARVALUE, '" . Config::DEFAULT_CONFIG_KEY . "') WHERE 1;" );
-            $this->addSql( 'ALTER TABLE oxconfig MODIFY COLUMN `OXVARVALUE` mediumblob;' );
-            $this->addSql( "UPDATE oxconfig SET `OXVARVALUE` = `OXVARVALUE_ENC` WHERE 1;" );
-            $this->addSql( 'ALTER TABLE oxconfig DROP COLUMN `OXVARVALUE_ENC`;' );
+            $this->addSql('ALTER TABLE oxuserpayments ADD COLUMN `OXVALUE_ENC` text;');
+            $this->addSql("UPDATE oxuserpayments SET `OXVALUE_ENC` = ENCODE(OXVALUE, '".Config::DEFAULT_CONFIG_KEY."') WHERE 1;");
+            $this->addSql('ALTER TABLE oxuserpayments MODIFY COLUMN `OXVALUE` mediumblob;');
+            $this->addSql("UPDATE oxuserpayments SET `OXVALUE` = `OXVALUE_ENC` WHERE 1;");
+            $this->addSql('ALTER TABLE oxuserpayments DROP COLUMN `OXVALUE_ENC`;');
         }
     }
 
