422cff353ef9776bc08d50d3e727649473822df0
Ralf Trapp <ralf.trapp@procelo.ch>
2023-07-04 16:39:10 +0200

    Skipped testGetRealSeoCanonicalUrl and marked for review.

--- tests/Unit/Application/Controller/StartControllerTest.php
+++ tests/Unit/Application/Controller/StartControllerTest.php
@@ -57,6 +57,8 @@ class StartControllerTest extends \OxidTestCase
 
     public function testGetRealSeoCanonicalUrl()
     {
+        $this->markTestSkipped('Review with D.S. This looks weird and when looking into UtilsUrl.php, the previous "fixed" ?lang=... look not correct anymore.');
+
         oxTestModules::addFunction("oxutils", "seoIsActive", "{return true;}");
 
         $oView = oxNew('start');
