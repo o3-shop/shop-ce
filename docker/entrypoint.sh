0c768138b3ada214646c113aa72df8b1a2ba027a
Nick Lorenz <nlo@tro.net>
2025-04-17 15:08:30 +0200

    Use entrypoint.sh

---
+++ docker/entrypoint.sh
@@ -0,0 +1,13 @@
+composer install --no-interaction --optimize-autoloader
+
+chown -R root:www-data /var/www/html/var && \
+chown -R root:www-data /var/www/html/source && \
+chown -R root:www-data /var/www/html/.env
+
+
+chmod -R 2775 /var/www/html/var && \
+chmod -R 2775 /var/www/html/source && \
+chmod -R 2775 /var/www/html/.env
+
+
+apache2-foreground
