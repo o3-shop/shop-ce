e0267ef0a5bc35b5634dd2f25952e27b7642c545
Nick Lorenz <nlo@tro.net>
2025-05-13 15:26:22 +0200

    Update Entrypoint.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,6 +2,17 @@ cp .env.example .env
 
 cp source/config.inc.php.dist source/config.inc.php
 
+wget https://github.com/o3-shop/wave-theme/archive/refs/heads/main.zip
+
+unzip main.zip
+
+cp -r wave-theme-main/out/* source/out/
+
+rm -rf wave-theme-main/out
+
+cp -r wave-theme-main/* source/Application/views/wave
+
+
 yes | composer install --no-interaction --optimize-autoloader
 
 chown -R root:www-data /var/www/html/var && \
