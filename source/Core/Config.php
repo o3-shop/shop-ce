dd56c3a020761af30d5fac1ec10ef4447d965199
Nick Lorenz | TroNet <nlo@tro.net>
2025-10-16 13:51:56 +0200

    Default Log Dir if Null (#53)
    * Fix getLogsDir to initialize sLogDir if null.
    
    * Build.
    
    * Workflow.
    
    * Add tests.
    
    ---------
    
    Co-authored-by: Nick Lorenz <me@xniicki.de>

--- source/Core/Config.php
+++ source/Core/Config.php
@@ -2190,6 +2190,10 @@ class Config extends \OxidEsales\Eshop\Core\Base
      */
     public function getLogsDir()
     {
+        if ($this->sLogDir === null) {
+            return $this->sLogDir = $this->getConfigParam('sShopDir') . 'log/';
+        }
+
         return $this->getConfigParam('sShopDir') . $this->sLogDir;
     }
 
