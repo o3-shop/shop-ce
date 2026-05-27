92937cfda080c91a8fe5b40fd5916db46fbcf017
Nick Lorenz | TroNet <nlo@tro.net>
2026-05-27 08:59:51 +0200

    fix: improve formatting of ExitHandler registration in bootstrap

--- source/bootstrap.php
+++ source/bootstrap.php
@@ -239,11 +239,11 @@ $configFile = new \OxidEsales\Eshop\Core\ConfigFile(OX_BASE_PATH . 'config.inc.p
  * Registry::set() (see tests/Unit/ExitHandlerTestTrait.php) so exit() calls
  * in the code under test do not tear down the PHPUnit process.
  */
-if(class_exists(\OxidEsales\Eshop\Core\ExitHandlerInterface::class)) {
-\OxidEsales\Eshop\Core\Registry::set(
-    \OxidEsales\Eshop\Core\ExitHandlerInterface::class,
-    new \OxidEsales\Eshop\Core\ExitHandler()
-);
+if (class_exists(\OxidEsales\Eshop\Core\ExitHandlerInterface::class)) {
+    \OxidEsales\Eshop\Core\Registry::set(
+        \OxidEsales\Eshop\Core\ExitHandlerInterface::class,
+        new \OxidEsales\Eshop\Core\ExitHandler()
+    );
 }
 /**
  * Ensure tmp directory exists for shop functionality.
